37fae21807237457bef184b7ccf6e09a5c267f69
BiancaWilkson <42818125+BiancaWilkson@users.noreply.github.com>
2025-11-22 14:51:29 -0500

    adds holopad.tsx

---
+++ tgui/packages/tgui/interfaces/Holopad.tsx
@@ -0,0 +1,5 @@
+import { useState } from 'react';
+import { Button, Icon, ImageButton, Input, Section, Stack } from 'tgui-core/components';
+
+import { useBackend } from '../backend';
+import { Window } from '../layouts';
